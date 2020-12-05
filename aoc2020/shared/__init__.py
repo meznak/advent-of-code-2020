597c0679551222e4d097911cecaab75f12115c14
Nathan Plamondon <nplamondon@gmail.com>
2020-12-04 20:54:31 -0700

    Add shared check_list function

--- aoc2020/shared/__init__.py
+++ aoc2020/shared/__init__.py
@@ -1,4 +1,5 @@
 import os
+from typing import Callable
 
 def read_input(script_file: str) -> ([], []):
     path = os.path.dirname(os.path.realpath(script_file))
@@ -10,3 +11,25 @@ def read_input(script_file: str) -> ([], []):
         data = [entry.strip() for entry in f.readlines()]
 
     return (sample, data)
+
+def check_list(sample: [str], data: [str], check: Callable[[list], int], \
+    sample_solution: int) -> int:
+    run_count = 0
+
+    for dataset in [sample, data]:
+        result = check(dataset)
+
+        if run_count == 0:
+            assert(result == sample_solution), \
+                f'''Expected {sample_solution}, got {result}'''
+            run_count += 1
+        else:
+            return result
+
+def run_checks(sample: list, data: list, check_functions: list, \
+    sample_solutions: list) -> None:
+
+    for index in [0]:
+        result = check_list(sample, data, check_functions[index], \
+            sample_solutions[index])
+        print(f'result {index}: {result}')

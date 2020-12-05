0b44e353c3bf7eb9559352ef5185264f295ffd88
Nathan Plamondon <nplamondon@gmail.com>
2020-12-04 21:45:25 -0700

    Lint shared library

--- aoc2020/shared/__init__.py
+++ aoc2020/shared/__init__.py
@@ -1,28 +1,41 @@
+'''
+Shared library for AoC2020 solutions
+'''
+
 import os
 from typing import Callable
 
 def read_input(script_file: str) -> (list, list, list):
+    '''
+    Read sample and puzzle input files
+    '''
+
     path = os.path.dirname(os.path.realpath(script_file))
 
-    with open(os.path.join(path, 'sample1'), 'r') as f:
-        sample1 = [entry.strip() for entry in f.readlines()]
+    with open(os.path.join(path, 'sample1'), 'r') as in_file:
+        sample1 = [entry.strip() for entry in in_file.readlines()]
 
     try:
-        with open(os.path.join(path, 'sample2'), 'r') as f:
-            sample2 = [entry.strip() for entry in f.readlines()]
+        with open(os.path.join(path, 'sample2'), 'r') as in_file:
+            sample2 = [entry.strip() for entry in in_file.readlines()]
     except FileNotFoundError:
         sample2 = None
         print('sample2 file not found')
 
     samples = [sample1, sample2]
 
-    with open(os.path.join(path, 'input'), 'r') as f:
-        data = [entry.strip() for entry in f.readlines()]
+    with open(os.path.join(path, 'input'), 'r') as in_file:
+        data = [entry.strip() for entry in in_file.readlines()]
 
     return (samples, data)
 
 def check_list(sample: [str], data: [str], check: Callable[[list], int], \
     sample_solution: int) -> int:
+    '''
+    Run solution against sample data, comparing expected results. If
+    satisfactory, run solution against puzzle data.
+    '''
+
     run_count = 0
 
     for dataset in [sample, data]:
@@ -32,11 +45,14 @@ def check_list(sample: [str], data: [str], check: Callable[[list], int], \
             assert(result == sample_solution), \
                 f'''Expected {sample_solution}, got {result}'''
             run_count += 1
-        else:
-            return result
+
+    return result
 
 def run_checks(samples: list, data: list, check_functions: list, \
     sample_solutions: list) -> None:
+    '''
+    Run all solutions for the current puzzle
+    '''
 
     for index in range(len(samples)):
         result = check_list(samples[index], data, check_functions[index], \

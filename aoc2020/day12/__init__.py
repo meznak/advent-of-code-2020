d33d4a9b158f4d47cc48ed784cba9e61149d51f2
Nathan Plamondon <nplamondon@gmail.com>
2020-12-12 22:03:38 -0700

    Solve day 12 part 2

--- aoc2020/day12/__init__.py
+++ aoc2020/day12/__init__.py
@@ -3,14 +3,14 @@ Advent of Code Day 12
 Rainy Risk
 '''
 
-SAMPLE_SOLUTIONS = [25]
+SAMPLE_SOLUTIONS = [25, 286]
 
 class Ship(object):
     '''A ferry'''
 
-    def __init__(self):
-        self.x_pos = 0
-        self.y_pos = 0
+    def __init__(self, position = (0,0)):
+        self.x_pos = position[0]
+        self.y_pos = position[1]
         self.direction = 90
 
     def turn(self, action: chr, value: int) -> None:
@@ -46,6 +46,32 @@ class Ship(object):
             elif self.direction == 270:
                 self.move('W', value)
 
+    def rotate(self, action: chr, value: int) -> None:
+        '''Rotate waypoint around a ship'''
+
+        while value > 0:
+            if action == 'R':
+                # Swap, negate y
+                temp = self.y_pos
+                self.y_pos = self.x_pos * -1
+                self.x_pos = temp
+            else:
+                # Negate y, swap
+                temp = self.y_pos * -1
+                self.y_pos = self.x_pos
+                self.x_pos = temp
+
+            value -= 90
+
+
+
+    def move_to_waypoint(self, value: int, waypoint: object) -> None:
+        '''Move ship to the waypoint'''
+
+        for _ in range(value):
+            self.x_pos += waypoint.x_pos
+            self.y_pos += waypoint.y_pos
+
 
 def parse_data(dataset: list) -> list:
     '''Interpret string data'''
@@ -73,6 +99,15 @@ def solve_1(dataset: list) -> int:
 def solve_2(dataset: list) -> int:
     '''Solve part 2'''
 
-    for item in dataset:
-        # TODO: Build solution
-        pass
+    ship = Ship()
+    waypoint = Ship((10, 1))
+
+    for action, value in dataset:
+        if action in ['N', 'E', 'S', 'W']:
+            waypoint.move(action, value)
+        elif action in ['R', 'L']:
+            waypoint.rotate(action, value)
+        elif action == 'F':
+            ship.move_to_waypoint(value, waypoint)
+
+    return abs(ship.x_pos) + abs(ship.y_pos)

b9e97597088dbda60fb0093adcd27bf3f0a45e2c
Nathan Plamondon <nplamondon@gmail.com>
2020-12-13 01:07:00 -0700

    Solve day 13 part 2?

--- aoc2020/day13/__init__.py
+++ aoc2020/day13/__init__.py
@@ -3,16 +3,21 @@ Advent of Code Day 13
 Shuttle Search
 '''
 
-SAMPLE_SOLUTIONS = [295]
+SAMPLE_SOLUTIONS = [295, 1068781]
 
 def parse_data(dataset: list) -> list:
     '''Interpret string data'''
 
-    output = []
+    output = [-1, []]
 
-    output.append(int(dataset[0]))
+    output[0] = int(dataset[0])
     buses = dataset[1].split(',')
-    output.append([int(bus_id) for bus_id in buses if bus_id != 'x'])
+
+    for bus_id in buses:
+        if bus_id != 'x':
+            output[1].append(int(bus_id))
+        else:
+            output[1].append(bus_id)
 
     return output
 
@@ -21,7 +26,8 @@ def solve_1(dataset: list) -> int:
 
     earliest = dataset[0]
     time = earliest
-    buses = dataset[1]
+    # strip x from the list of buses
+    buses = [bus_id for bus_id in dataset[1] if bus_id != 'x']
 
     while True:
         for bus_id in buses:
@@ -33,6 +39,30 @@ def solve_1(dataset: list) -> int:
 def solve_2(dataset: list) -> int:
     '''Solve part 2'''
 
-    for item in dataset:
-        # TODO: Build solution
-        pass
+    buses = list(enumerate(dataset[1]))
+    buses = [bus for bus in buses if bus[1] != 'x']
+
+    if len(buses) > 10:
+        earliest = 100000000000000
+    else:
+        earliest = 1000000
+    time_step = max([bus[1] for bus in buses])
+
+    for bus in buses:
+        if bus[1] == time_step:
+            offset = (earliest + bus[1]) % bus[1]
+            start_time = earliest + 2 * bus[1] - offset - bus[0]
+            break
+
+    found = False
+
+    while not found:
+        for bus in buses:
+            if (start_time + bus[0]) % bus[1] != 0:
+                start_time += time_step
+                break
+            if bus == buses[-1]:
+                found = True
+                break
+
+    return start_time

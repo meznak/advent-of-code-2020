b32d0054a52b7852bc27acf06e44893d0bb314df
Nathan Plamondon <nplamondon@gmail.com>
2020-12-14 23:46:11 -0700

    Solve day 15 part 1

---
+++ aoc2020/day15/__init__.py
@@ -0,0 +1,53 @@
+'''
+Advent of Code Day 15
+Rambunctious Recitation
+'''
+
+SAMPLE_SOLUTIONS = [436]
+
+def parse_data(dataset: list) -> list:
+    '''Interpret string data'''
+
+    output = []
+
+    for item in dataset:
+        if item != '':
+            output = [int(num) for num in item.split(',')]
+
+    return output
+
+def solve_1(dataset: list) -> int:
+    '''Solve part 1'''
+
+    turn = 1
+    numbers = {}
+    new_number = None
+
+    for new_number in dataset:
+        numbers[new_number] = [turn]
+        turn += 1
+
+    while turn <=2020:
+        number = new_number
+
+        if len(numbers[number]) == 1:
+            new_number = 0
+        else:
+            new_number = numbers[number][1] - numbers[number][0]
+
+        if new_number not in numbers:
+            numbers[new_number] = [turn]
+        else:
+            numbers[new_number].append(turn)
+            numbers[new_number] = numbers[new_number][-2:]
+
+        turn += 1
+
+    return new_number
+
+def solve_2(dataset: list) -> int:
+    '''Solve part 2'''
+
+    for item in dataset:
+        # TODO: Build solution
+        pass
